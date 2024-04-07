9dfde3890a67fff996d07d0a335633525825f6fb
Rishabh Suryavanshi <rishabhsuryavanshi2004@gmail.com>
2024-04-07 16:21:42 +0530

    Update Home.js

--- medicoin/src/app/Componentts/Home.js
+++ medicoin/src/app/Componentts/Home.js
@@ -32,7 +32,7 @@ const Home = () => {
     
     <div className="flex flex-row gap-10">
       <Link href="/Campaign">
-      <button className="bg-none text-blueSecondary border border-blueSecondary px-4 py-2 rounded-2xl hover:bg-blue hover:text-rishabh  hover:border-rishabh transition duration-300  font-bold font-aldrich text-xl">FUND A CAMPAIGN\</button>
+      <button className="bg-none text-blueSecondary border border-blueSecondary px-4 py-2 rounded-2xl hover:bg-blue hover:text-rishabh  hover:border-rishabh transition duration-300  font-bold font-aldrich text-xl">FUND A CAMPAIGN</button>
       </Link>
     
     <button className="bg-none text-blueSecondary border border-blueSecondary px-4 py-2 rounded-2xl hover:bg-blue hover:text-rishabh hover:border-rishabh transition duration-300  font-bold font-aldrich text-xl"  >FUND A  RESEARCH CAMPAIGN</button>
